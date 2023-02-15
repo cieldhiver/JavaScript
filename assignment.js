0292b66e24fcfbbb83fbbe1ecbc97ae560aec834
cieldhiver <kiminha1030@gmail.com>
2023-02-16 02:08:39 +0900

    javascript fundamental(1)

---
+++ assignment.js
@@ -0,0 +1,9 @@
+/*
+let country = 'korea';
+let continent = 'pacific';
+let population = 5000;
+
+console.log(country)
+console.log(continent)
+console.log(population)
+*/
---
+++ script.js
@@ -0,0 +1,84 @@
+/*
+let js = 'amazing';
+console.log(20 + 30 - 15);
+
+console.log("Mina");
+console.log(20);
+
+let firstName = "Celine";
+console.log(firstName);
+
+
+let javasrciptIsFun = true;
+console.log(javasrciptIsFun);
+
+//console.log(typeof true);
+console.log(typeof javasrciptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Mina');
+
+javasrciptIsFun = 'Yes!';
+console.log(typeof javasrciptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 2023;
+console.log(typeof year);
+
+console.log(typeof null);
+
+
+let age = 20;
+age = 21;
+
+
+const birthYear = 1996;
+//birthYear = 1990;
+
+
+var job = 'programmer'
+job = 'teacher'
+
+const now = 2037;
+const ageMina = now - 1991;
+const ageNina = now - 2001;
+console.log(ageMina, ageNina);
+
+// 2**3 = 2*2*2 
+console.log(ageMina * 2, ageNina / 2, 2 ** 3)
+
+const firstName = 'Kim'
+const lastName = 'minha'
+console.log(firstName + ' ' + lastName);
+
+//Assignment operators
+let x = 10 + 5; //15
+x += 10; // x = x+10 = 25
+x *= 10; // x = x*10 = 250
+x++; // x = x+1 = 251
+x--; // x = x-1 = 250
+console.log(x);
+
+//Comparison operators
+console.log(ageMina > ageNina); //>, <, >=, <=
+console.log(ageMina >= 30)
+
+const isFullAge = ageMina >= 30;
+
+console.log(now - 1991 > now - 2001)
+*/
+
+const now = 2037;
+const ageMina = now - 1991;
+const ageNina = now - 2001;
+
+console.log(now - 1991 > now - 2001)
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y)
+
+const averageAge = (ageMina + ageNina) / 2;
+console.log(ageMina, ageNina, averageAge)
