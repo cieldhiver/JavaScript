5828aa1cd7b2c14625ff7df5d86fa79fa1606dbb
cieldhiver <kiminha1030@gmail.com>
2023-02-19 18:19:57 +0900

    functions

--- script2.js
+++ script2.js
@@ -1,1 +1,35 @@
 'use strict';
+/*
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriverLicense = true; //변수에 s를 생략시켜 의도적으로 오류를 발생시킨다
+if (hasDriversLicense) console.log('I can drive');
+
+//const interface = 'Audio';
+//const private = 534;
+// -> Uncaught SyntaxError: Unexpected strict mode reserved word 
+*/
+
+//Functions
+function logger() {
+    console.log('My name is mina');
+}
+
+//calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    //console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+//console.log(fruitProcessor(5, 0));
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice)
