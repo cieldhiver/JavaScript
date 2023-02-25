a46a934b991b8675a50e6eb0481e8be032d171c8
cieldhiver <kiminha1030@gmail.com>
2023-02-25 23:45:23 +0900

    메소드

--- script2.js
+++ script2.js
@@ -193,15 +193,97 @@ console.log(friends.includes(23)); //true
 if (friends.includes("Peter")) {
   console.log("You have a friend called Peter");
 }
-*/
+
 // Coding Challenge #2
-const calcTip = function (bill) {
+
+function calcTip(bill) {
   return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
-};
-// const calcTip = bill => bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+//const calcTip = bill => bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 
 const bills = [125, 555, 44];
 const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
 const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
-
 console.log(bills, tips, totals);
+
+// Objects
+// Dot vs. Bracket Notation(괄호 표기법)
+const jonas = {
+  firstName: "Jonas",
+  lastName: "Schmedtmann",
+  age: 2037 - 1991,
+  job: "teacher",
+  friends: ["Michael", "Peter", "Steven"],
+};
+
+console.log(jonas.lastName);
+console.log(jonas["lastName"]);
+
+const nameKey = "Name";
+console.log(jonas["first" + nameKey]); //Jonas
+console.log(jonas["last" + nameKey]); //Schmedtmann
+
+const interestedIn = prompt(
+  "What do you want to know about Jonas? Choose between firstName, lastName, age, job, and friends"
+);
+//console.log(jonas[interestedIn]);
+
+if (jonas[interestedIn]) {
+  console.log(jonas[interestedIn]);
+} else {
+  console.log("Wrong request!");
+}
+
+jonas.location = "Portugal";
+jonas["twitter"] = "@hihi";
+console.log(jonas);
+
+// Challenge
+// "Jonas has 3 friends, and his best friend is called Michael"
+console.log(
+  `${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`
+);
+*/
+// Object Methods
+const jonas = {
+  firstName: "Jonas",
+  lastName: "Schmedtmann",
+  birthYear: 1991,
+  job: "teacher",
+  friends: ["Michael", "Peter", "Steven"],
+  hasDriversLicense: true,
+  //개체 속성으로 function값 가능
+  //#1
+  // calcAge: function (birthYear) {
+  //   return 2037 - birthYear;
+  // }
+
+  //#2
+  // calcAge: function () {
+  //   //console.log(this); //this는 jonas의 모든 객체
+  //   return 2037 - this.birthYear;
+  // }
+
+  //#3
+  calcAge: function () {
+    //새로운 속성 추가
+    this.age = 2037 - this.birthYear;
+    return this.age;
+  },
+
+  getSummary: function () {
+    return `${this.firstName} is a ${this.calcAge()}-year old ${
+      this.job
+    }, and he has ${this.hasDriversLicense ? "a" : "no"} driver's license.`;
+  },
+};
+
+console.log(jonas.calcAge());
+console.log(jonas.age);
+console.log(jonas.age);
+console.log(jonas.age);
+//console.log(jonas["calcAge"](1991));
+
+// Challenge
+// "Jonas is a 46-year old teacher, and he has a driver's license"
+console.log(jonas.getSummary());
