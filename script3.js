8ab63f99a6aaaaf28023321a894d68e58dd8677a
cieldhiver <kiminha1030@gmail.com>
2023-03-09 23:48:19 +0900

    hoist, this

--- script3.js
+++ script3.js
@@ -1,7 +1,7 @@
 'use strict';
 //<JavaScript Works Behind the Scenes>
 // Scoping in Practice
-
+/*
 function calcAge(birthYear) {
   const age = 2037 - birthYear;
 
@@ -38,3 +38,83 @@ const firstName = 'Jonas';
 calcAge(1991);
 // console.log(age);
 // printAge();
+
+// Hoisting and TDZ in Practice
+
+//변수
+console.log(me);
+//console.log(job);
+//console.log(year);
+
+var me = 'Jonas';
+let job = 'teacher';
+const year = 1991;
+
+//함수
+console.log(addDecl(2, 3));
+//console.log(addExpr(2, 3));
+console.log(addArrow);
+//console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+//Example
+console.log(numProducts); //undefined
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products deleted!');
+}
+
+//결론 : let, const 사용(var 사용x) + 선언 후 함수 작성
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x); //true
+console.log(y === window.y); //false
+console.log(z === window.z); //false
+// var로 선언한 변수는 window 객체에 속성값을 가진다.
+*/
+// This Keyword in Practice
+console.log(this);
+
+const calcAge = function (birthYear) {
+  console.log(2037 - birthYear);
+  console.log(this); //undefined
+};
+calcAge(1991);
+
+const calcAgeArrow = birthYear => {
+  console.log(2037 - birthYear);
+  console.log(this); //window
+};
+calcAgeArrow(1980);
+
+const jonas = {
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+  },
+};
+jonas.calcAge();
+
+const matilda = {
+  year: 2017,
+};
+matilda.calcAge = jonas.calcAge;
+matilda.calcAge();
+
+const f = jonas.calcAge;
+f(); //undefined
